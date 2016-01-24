5ffab79d1cb4821c574848189b23ae768e20c554
sdw1314 <1056127878@qq.com>
2016-01-24 18:12:50 +0800

    add clor
    add some color

--- serachMap-1-20/ViewController.swift
+++ serachMap-1-20/ViewController.swift
@@ -10,9 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var bt1: UIButton!
+
+    @IBOutlet weak var bt2: UIButton!
+  
+    @IBOutlet weak var bt3: UIButton!
+    
+    @IBOutlet weak var btMenu: UIButton!
+
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //对三个按钮进行设置
+        self.bt1.alpha = 0
+        self.bt2.alpha = 0
+        self.bt3.alpha = 0
+        
+
+        //透明度变为零
+        self.btMenu.alpha = 0
+        UIView.animateWithDuration(1, delay: 1, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            self.btMenu.alpha = 1
+            //旋转角度，90度
+            self.btMenu.transform = CGAffineTransformMakeRotation(0.25*3.1415927)
+        }, completion: nil )
+
+    }
+    
+    @IBAction func btnMenuClick(sender: UIButton) {
+        
+        UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            self.bt1.alpha = 0.8
+            //按钮移动
+            self.bt1.transform = CGAffineTransformConcat(CGAffineTransformMakeScale(1.5, 1.5), CGAffineTransformMakeTranslation(-80, -25))
+            self.bt2.alpha = 0.8
+            self.bt2.transform = CGAffineTransformConcat(CGAffineTransformMakeScale(1.5, 1.5), CGAffineTransformMakeTranslation(0, -50))
+            self.bt3.alpha = 0.8
+            self.bt3.transform = CGAffineTransformConcat(CGAffineTransformMakeScale(1.5, 1.5), CGAffineTransformMakeTranslation(80, -25))
+            
+            self.btMenu.transform = CGAffineTransformMakeRotation(0)
+            
+            
+            }, completion: nil )
     }
 
     override func didReceiveMemoryWarning() {
